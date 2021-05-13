d0b81d8675e337da516c5ed9d5aa779c27705cd5
LinYouXin <310467466@qq.com>
2021-05-13 16:58:27 +0800

    -

--- app/Services/YwKyy/PushChannelService.php
+++ app/Services/YwKyy/PushChannelService.php
@@ -5,6 +5,8 @@ namespace App\Services\YwKyy;
 
 use App\Common\Enums\CpTypeEnums;
 use App\Common\Enums\ProductTypeEnums;
+use App\Common\Services\ErrorLogService;
+use App\Common\Tools\CustomException;
 use App\Models\ConfigModel;
 use App\Services\PushChannelBaseService;
 use App\Spiders\Yw\YwSpider;
@@ -51,51 +53,65 @@ class PushChannelService extends PushChannelBaseService
             $count = $channelList['count'];
             $currentCount += count($channelList['list']);
             foreach ($channelList['list'] as $item){
-                $unionForceChapter = $unionChapter = [];
 
-                $book = $spider->getBookInfo($item['cbid']);
-                //创建书籍
-                $unionBook = $this->unionApiService->apiCreateBook([
-                    'cp_type'    => $product['cp_type'],
-                    'cp_book_id' => $book['bookInfo']['cbid'],
-                    'name'       => $book['bookInfo']['BookName'],
-                    'author_name'  => '',
-                    'all_words'  => 0,
-                    'update_time'  => $book['bookInfo']['NewChapterTime'],
-                ]);
-                // 创建章节
-                foreach ($book['chapterList'] as $chapter){
-                    if(!isset($chapter['id'])) continue;
-                    $tmpChapter = $this->unionApiService->apiCreateChapter([
-                        'book_id' => $unionBook['id'],
-                        'cp_chapter_id' => $chapter['ccid'],
-                        'name'  => $chapter['chapterName'],
-                        'seq'  =>  $chapter['id'] ?? 0
+                try{
+                    $unionForceChapter = $unionChapter = [];
+
+                    $book = $spider->getBookInfo($item['cbid']);
+                    //创建书籍
+                    $unionBook = $this->unionApiService->apiCreateBook([
+                        'cp_type'    => $product['cp_type'],
+                        'cp_book_id' => $book['bookInfo']['cbid'],
+                        'name'       => $book['bookInfo']['BookName'],
+                        'author_name'  => '',
+                        'all_words'  => 0,
+                        'update_time'  => $book['bookInfo']['NewChapterTime'],
                     ]);
+                    // 创建章节
+                    foreach ($book['chapterList'] as $chapter){
+                        if(!isset($chapter['id'])) continue;
 
-                    //打开章节
-                    if($chapter['ccid'] == $item['ccid']){
-                        $unionChapter = $tmpChapter;
+                        $tmpChapter = $this->unionApiService->apiCreateChapter([
+                            'book_id' => $unionBook['id'],
+                            'cp_chapter_id' => $chapter['ccid'],
+                            'name'  => $chapter['chapterName'],
+                            'seq'  =>  $chapter['id'] ?? 0
+                        ]);
+
+                        //打开章节
+                        if($chapter['ccid'] == $item['ccid']){
+                            $unionChapter = $tmpChapter;
+                        }
+
+                        //强制章节
+                        if($chapter['id'] == $item['force_chapter']){
+                            $unionForceChapter = $tmpChapter;
+                        }
                     }
 
-                    //强制章节
-                    if($chapter['id'] == $item['force_chapter']){
-                        $unionForceChapter = $tmpChapter;
-                    }
+                    // 创建渠道
+                    $this->unionApiService->apiCreateChannel([
+                        'name'       => $item['name'],
+                        'product_id' => $product['id'],
+                        'cp_channel_id' => $item['id'],
+                        'book_id'  => $unionBook['id'],
+                        'chapter_id'  => $unionChapter['id'],
+                        'force_chapter_id'  => $unionForceChapter['id'],
+                        'create_time'  => $item['create_time'],
+                        'updated_time'  => $item['create_time'],
+                    ]);
+                }catch(CustomException $e){
+                    //日志
+                    (new ErrorLogService())->catch($e);
+
+                    $errInfo = $e->getErrorInfo(true);
+                    echo $errInfo['message']. "\n";
+                }catch(\Exception $e){
+                    //日志
+                    (new ErrorLogService())->catch($e);
+
+                    echo $e->getMessage(). "\n";
                 }
-
-                // 创建渠道
-                $this->unionApiService->apiCreateChannel([
-                    'name'       => $item['name'],
-                    'product_id' => $product['id'],
-                    'cp_channel_id' => $item['id'],
-                    'book_id'  => $unionBook['id'],
-                    'chapter_id'  => $unionChapter['id'],
-                    'force_chapter_id'  => $unionForceChapter['id'],
-                    'create_time'  => $item['create_time'],
-                    'updated_time'  => $item['create_time'],
-                ]);
-
             }
             $page += 1;
         }while($currentCount < $count);
