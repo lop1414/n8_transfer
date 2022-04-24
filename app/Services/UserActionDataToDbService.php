15279ed7681b877430e8cff049155b8abb0d216d
linyouxin <yllobx@outlook.com>
2022-04-24 15:40:58 +0800

    -

--- app/Services/UserActionDataToDbService.php
+++ app/Services/UserActionDataToDbService.php
@@ -5,11 +5,13 @@ namespace App\Services;
 use App\Common\Enums\ReportStatusEnum;
 use App\Common\Helpers\Functions;
 use App\Common\Services\BaseService;
+use App\Common\Services\ErrorLogService;
+use App\Common\Tools\CustomException;
 use App\Common\Tools\CustomQueue;
 use App\Enums\QueueEnums;
-use App\Enums\UserActionTypeEnum;
 use App\Models\UserActionLogModel;
 use App\Traits\UserAction\AddShortcut;
+use Illuminate\Support\Facades\DB;
 
 class UserActionDataToDbService extends BaseService
 {
@@ -43,38 +45,57 @@ class UserActionDataToDbService extends BaseService
 
         $productService = (new ProductService())->setMap();
 
-        $queue->openTransaction();
-        $queue->setConsumeHook(function ($data) use ($productService){
-            $product = $productService->readByMap($data['cp_type'],$data['cp_product_alias']);
-            $data['product_id'] = $product['id'];
-            $data['status'] = ReportStatusEnum::WAITING;
-            $data['matcher'] = $product['matcher'];
+        $rePushData = [];
+        while ($data = $queue->pull()) {
 
+            try{
+                DB::beginTransaction();
+                $product = $productService->readByMap($data['cp_type'],$data['cp_product_alias']);
+                $data['product_id'] = $product['id'];
+                $data['status'] = ReportStatusEnum::WAITING;
+                $data['matcher'] = $product['matcher'];
 
-            if($data['type'] == UserActionTypeEnum::ADD_SHORTCUT){
-                if($this->isRepeatAddShortcut($data)){
-                    echo "重复加桌\n";
-                    return;
+                (new UserActionLogModel())
+                    ->setTableNameWithMonth($data['action_time'])
+                    ->create($data);
+
+                DB::commit();
+
+            }catch (CustomException $e){
+                DB::rollBack();
+
+                //日志
+                (new ErrorLogService())->catch($e);
+
+                // 重回队列
+                $data['exception'] = $e->getErrorInfo();
+                $data['code'] = $e->getCode();
+                $rePushData[] = $data;
+
+            }catch (\Exception $e){
+                DB::rollBack();
+
+                if($e->getCode() == 23000){
+                    echo "  命中唯一索引 \n";
+                    continue;
                 }
+
+                //日志
+                (new ErrorLogService())->catch($e);
+
+                // 重回队列
+                $data['exception'] = $e->getMessage();
+                $data['code'] = $e->getCode();
+                $rePushData[] = $data;
+
             }
+        }
 
-            (new UserActionLogModel())
-                ->setTableNameWithMonth($data['action_time'])
-                ->create($data);
-
-            // 加桌记录log
-            if($data['type'] == UserActionTypeEnum::ADD_SHORTCUT){
-                $this->setAddShortcutCacheLog($data);
-            }
-        });
-
-        $queue->setExceptionHook(function ($data,$e){
-            if($e->getCode() == 23000){
-                echo "  命中唯一索引 \n";
-            }
-        });
-
-        $queue->consume();
+        foreach($rePushData as $item){
+            // 重推
+            $queue->setItem($item);
+            $queue->rePush();
+        }
     }
 
 
