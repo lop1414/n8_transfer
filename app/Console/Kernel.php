2236b82621d0f3c58256a6481313a988adbdd94d
linyouxin <yllobx@outlook.com>
2022-06-22 10:02:10 +0800

    番茄快应用

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -69,7 +69,8 @@ class Kernel extends ConsoleKernel
         $tenMinuteRange = "'{$tenMinuteFront}','{$dateTime}'";
         //半小时区间
         $halfHourRange = "'".date('Y-m-d H:i:s',TIMESTAMP-60*30)."','{$dateTime}'";
-
+        //一小时区间
+        $oneHourRange = "'{$oneHourFront}','{$dateTime}'";
         //前5分钟区间
         $frontFiveMinuteRange = "'{$tenMinuteFront}','{$fiveMinuteFront}'";
 
@@ -88,7 +89,7 @@ class Kernel extends ConsoleKernel
         $schedule->command("sync_user_action --action_type=REG --cp_type=TW --product_type=APP --time={$fiveMinuteRange}")->cron('*/5 * * * *');
         $schedule->command("sync_user_action --action_type=REG --cp_type=TW --product_type=KYY --time={$fiveMinuteRange}")->cron('*/5 * * * *');
 //        $schedule->command("sync_user_action --action_type=REG --cp_type=QY --product_type=H5  --time={$fiveMinuteRange}")->cron('*/5 * * * *');
-        $schedule->command("sync_user_action --action_type=REG --cp_type=FQ --product_type=KYY --time={$halfHourRange}")->cron('*/10 * * * *');
+        $schedule->command("sync_user_action --action_type=REG --cp_type=FQ --product_type=KYY --time={$oneHourRange}")->cron('*/10 * * * *');
         $schedule->command("sync_user_action --action_type=REG --cp_type=BM --product_type=KYY --time={$fiveMinuteRange}")->cron('*/5 * * * *');
         $schedule->command("sync_user_action --action_type=REG --cp_type=ZY --product_type=KYY --time={$fiveMinuteRange} --time_interval=21600 ")->cron('*/5 * * * *');
 
