0f02b462ef07d36d4c0f5fc2224ea0b3baeb3987
linyouxin <yllobx@outlook.com>
2022-07-23 10:01:35 +0800

    -

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -92,9 +92,9 @@ class Kernel extends ConsoleKernel
 //        $schedule->command("sync_user_action --action_type=REG --cp_type=FQ --product_type=KYY --time={$oneHourRange}")->cron('*/10 * * * *');
         $schedule->command("sync_user_action --action_type=REG --cp_type=BM --product_type=KYY --time={$fiveMinuteRange}")->cron('*/5 * * * *');
         $schedule->command("sync_user_action --action_type=REG --cp_type=ZY --product_type=KYY --time={$fiveMinuteRange} --time_interval=21600 ")->cron('*/5 * * * *');
-        $schedule->command("sync_user_action --action_type=REG --cp_type=MB --product_type=KYY --time={$fiveMinuteRange} --time_interval=21600 ")->cron('*/5 * * * *');
+        $schedule->command("sync_user_action --action_type=REG --cp_type=MB --product_type=DY_MINI_PROGRAM --time={$fiveMinuteRange} ")->cron('*/5 * * * *');
 
-        $schedule->command("sync_user_action --action_type=ADD_SHORTCUT --cp_type=BM --product_type=DY_MINI_PROGRAM --time={$fiveMinuteRange}")->cron('*/5 * * * *');
+        $schedule->command("sync_user_action --action_type=ADD_SHORTCUT --cp_type=BM --product_type=KYY --time={$fiveMinuteRange}")->cron('*/5 * * * *');
 
         $schedule->command("sync_user_action --action_type=ORDER --time={$tenMinuteRange}")->cron('*/5 * * * *');
 
